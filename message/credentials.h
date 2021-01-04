53e4a440f5da6ca7b27fd16ba8e4bd33b78c6711
HerbGlrt <62862399+HerbGlrt@users.noreply.github.com>
2021-01-04 19:11:40 -0300

    Update credentials.h

--- message/credentials.h
+++ message/credentials.h
@@ -1,3 +1,7 @@
-const char* _ssid = "WLAN_NAME";
-const char* _password = "PASSWORD";
-const String _url = "PATH_tO_YOUR_GIST";
+const char* _ssid = "WiFi ID";
+const char* _password = "WiFi Password";-
+
+const String _url = "/Your-Github-username/Hash-Code/raw";
+//Copy your Gist url from the share option, then delete the "https://gist.github.com" part from it.
+//Then you will need to add "/raw" at the end of the url.
+//This should leave you with something like "/Your-Github-username/Hash-Code/raw"
